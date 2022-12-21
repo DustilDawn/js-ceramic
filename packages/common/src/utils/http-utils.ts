7fc0a4aeef254991b77c81f80f54a932a43c6666
Dustil Dawn <dustildawn@proton.me>
2022-12-21 00:31:32 +0000

    update

--- packages/common/src/utils/http-utils.ts
+++ packages/common/src/utils/http-utils.ts
@@ -1,7 +1,7 @@
-import fetch from 'cross-fetch'
-import { mergeAbortSignals, TimedAbortSignal, abortable } from './abort-signal-utils.js'
+// // import fetch from 'cross-fetch'
+// import { mergeAbortSignals, TimedAbortSignal, abortable } from './abort-signal-utils.js'
 
-const DEFAULT_FETCH_TIMEOUT = 60 * 1000 * 3 // 3 minutes
+// const DEFAULT_FETCH_TIMEOUT = 60 * 1000 * 3 // 3 minutes
 interface FetchOpts {
   body?: any
   method?: string
@@ -18,21 +18,8 @@ export async function fetchJson(url: URL | string, opts: FetchOpts = {}): Promis
     })
   }
 
-  const timeoutLength = opts.timeout || DEFAULT_FETCH_TIMEOUT
-  const timedAbortSignal = new TimedAbortSignal(timeoutLength)
+  const res = fetch(String(url), { ...opts, credentials: 'include' })
 
-  const signal = opts.signal
-    ? mergeAbortSignals([opts.signal, timedAbortSignal.signal])
-    : timedAbortSignal.signal
-
-  const res = await abortable(signal, (abortSignal) => {
-    return fetch(String(url), { ...opts, signal: abortSignal, credentials: 'include' })
-  }).finally(() => timedAbortSignal.clear())
-
-  if (!res.ok) {
-    const text = await res.text()
-    throw new Error(`HTTP request to '${url}' failed with status '${res.statusText}': ${text}`)
-  }
-
+  // @ts-ignore
   return res.json()
 }
